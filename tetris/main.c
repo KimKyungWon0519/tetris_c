725db4d6f21fb8c69d360fe57bc5fddc77d81646
KimKyungWon0519 <gwon080870@gmail.com>
2021-04-15 19:54:44 +0900

    add random tetromino

--- tetris/main.c
+++ tetris/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "game.h"
 
 int main() {
-	printBackground();
-	printTetromino(4, 1, 6);
+	int x = 0;
+	int random = 0;
+
+	while (1) {
+		if (x == 0) {
+			random = rand() % 7;
+		}
+		
+		printTetromino(4, 1 + x++, random);
+		
+		if (x == 18) {
+			x = 0;
+		}
+		Sleep(50);
+	}
 }
